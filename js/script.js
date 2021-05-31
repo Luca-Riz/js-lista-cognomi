185a04db50399bcd3d5a09138298632ece32cdc8
Luca-Riz <luca983@gmail.com>
2021-05-31 15:32:59 +0200

    add -if in else if...

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // Mail
 //  1.  Chiedi all’utente la sua email,
 //  2.  controlla che sia nella lista di chi può accedere,
@@ -7,7 +8,7 @@
 
 // creo una lista di e-mail da verificare
 
-var eMailValide = ['pinco@mail.com','pallo@mail.com','toni@mail.com','bepi@mail.com','menego@mail.com',]
+var eMailValide = ['pinco@mail.com', 'pallo@mail.com', 'toni@mail.com', 'bepi@mail.com', 'menego@mail.com']
 
 // chiedo la mail all'utente
 
@@ -15,10 +16,10 @@ var eMailUser = prompt('Inserisci la tua e-mail')
 
 // controllo che sia sulla lista e stampo un messaggio in base al risultato della verifica
 
-for(var i = 0; i < eMailValide.length; i++){
+for (var i = 0; i < eMailValide.length; i++) {
     if (eMailValide[i] == eMailUser) {
         console.log('mail nella lista');
-    }  else if {
+    }  else {
         console.log('mail non valida');
     }
 }
@@ -36,3 +37,5 @@ for(var i = 0; i < eMailValide.length; i++){
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 // Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
+
+
