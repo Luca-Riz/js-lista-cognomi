bea4a36328984e36c3eefbadd67d1997a3cddc97
Luca-Riz <luca983@gmail.com>
2021-05-31 16:55:25 +0200

    add apici corretti...

--- js/script.js
+++ js/script.js
@@ -29,32 +29,3 @@ if ( ok != true) {
     document.getElementById('mailok').innerHTML += ' non presente nella lista ammessa, riprovare.';
 }
 
-
-//==============================================================================
-
-// Lista Cognomi:
-// chiedi all’utente il cognome
-// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-// stampa la lista ordinata alfabeticamente
-// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-// Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
-
-//------------------------------------------------------------------
-
-// creo array
-
-var cognomi [Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’]
-
-// chiedo all’utente il cognome e lo inserisco nell'array
-
-
-
-// stampo la lista ordinata alfabeticamente
-
-
-
-// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-
-
-// Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
-
