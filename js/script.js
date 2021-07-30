9ae96d5bc9eed5bdd526b405cf35b8a0995abd04
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-30 18:06:44 +0200

    asdsdasd

--- js/script.js
+++ js/script.js
@@ -8,11 +8,11 @@
 
 // creo una lista di e-mail da verificare
 
-var eMailValide = ['pinco@mail.com', 'pallo@mail.com', 'toni@mail.com', 'bepi@mail.com', 'menego@mail.com']
+var eMailValide = ['pinco@mail.com', 'pallo@mail.com', 'toni@mail.com', 'bepi@mail.com', 'menego@mail.com'];
 
 // chiedo la mail all'utente
 
-var eMailUser = prompt('Inserisci la tua e-mail')
+var eMailUser = prompt('Inserisci la tua e-mail');
 
 // controllo che sia sulla lista e stampo un messaggio in base al risultato della verifica
 
@@ -25,7 +25,7 @@ for (var i = 0; i < eMailValide.length; i++) {
     }
 }
 
-if ( ok != true) {
+if (ok != true) {
     document.getElementById('mailok').innerHTML += ' non presente nella lista ammessa, riprovare.';
 }
 
